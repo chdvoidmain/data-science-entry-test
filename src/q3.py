d0c79711f1213f2b30c6f00808f42c7a1df8ed63
chdvoidmain <31386446+chdvoidmain@users.noreply.github.com>
2026-02-22 16:49:28 +0800

    Add files via upload

---
+++ src/q3.py
@@ -0,0 +1,21 @@
+def update_dictionary(dct, key, value):
+    # check if key exists in dct dictionary
+    if key in dct:
+        # if yes print existing value
+        print("Original value:", dct[key])
+    # update key with new value
+    dct[key] = value
+    # return the new updated dct dictionary
+    return dct
+
+
+# Task 2
+# Scenario 1
+dct1 = {}
+result1 = update_dictionary(dct1, "name", "Alice")
+print(result1)
+
+dct2 = {"age": 25}
+# Scenario 2
+result2 = update_dictionary(dct2, "age", 26)
+print(result2)
